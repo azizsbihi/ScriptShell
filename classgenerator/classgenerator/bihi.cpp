31b5e801cc8f7bc959e5618c257c7ceb3a341dfa
azizsbihi <mohamedaziz.sebihi@esprit.tn>
2023-11-19 23:32:16 +0100

    classGener

---
+++ classgenerator/classgenerator/bihi.cpp
@@ -0,0 +1,17 @@
+
+*********************************
+
+           CopyRight aziz
+
+**********************************
+/*
+author : aziz
+date : 19 نوفمبر, 2023 CET 06:32:30 م
+brief:
+*/
+#include "bihi
+namespace :  aze{
+    bihi::bihi(){}
+    ::~(){}
+}
+
